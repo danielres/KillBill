68f00acaeb635bd4fc49d5657b1421c66af8b8dd
Daniel Reszka <dannn.r@gmail.com>
2013-08-07 21:46:58 +0200

    more refactoring of cucumber tests

--- features/invoice.feature
+++ features/invoice.feature
@@ -9,17 +9,17 @@ Feature: Generate an invoice with activvities and totals
   Background:
 
   Scenario: A user reads the total hours worked on an invoice with two activities
-    Given an invoice with number "2013012"
-    And   an activity "yawning" counting 32.5 hours for the invoice "2013012"
-    And   an activity "snoring" counting 11.1 hours for the invoice "2013012"
-    Then  the total hours for invoice "2013012" should be 43.6
+    Given an invoice with number 12
+    And   an activity "yawning" counting 32.5 hours for the invoice 12
+    And   an activity "snoring" counting 11.1 hours for the invoice 12
+    Then  the total hours for invoice 12 should be 43.6
 
   Scenario: A user reads how much he is charged, without and with VAT
-    Given an invoice "2013013" with VAT: 21%, hourly rate: 56
-    And   an activity "yawning" counting 2 hours for the invoice "2013013"
-    And   an activity "snoring" counting 3.1 hours for the invoice "2013013"
-    Then  the total charged for invoice "2013013" without taxes should be 285.60
-    And   the taxes for invoice "2013013" should be 59.98
-    And   the total charged for invoice "2013013" with taxes should be 345.58
+    Given an invoice with number 13, VAT: 21%, hourly rate: 56
+    And   an activity "yawning" counting 2 hours for the invoice 13
+    And   an activity "snoring" counting 3.1 hours for the invoice 13
+    Then  the total charged for invoice 13 without taxes should be 285.60
+    And   the taxes for invoice 13 should be 59.98
+    And   the total charged for invoice 13 with taxes should be 345.58
 
 
--- features/step_definitions/invoice_steps.rb
+++ features/step_definitions/invoice_steps.rb
@@ -1,37 +1,45 @@
 require_relative "../../models/invoice"
 require 'cucumber/rspec/doubles'
 
-CAPTURE_FLOAT = Transform /^[0-9]*\.?[0-9]+$/ do |float|
+FLOAT = Transform /^[0-9]*\.?[0-9]+$/ do |float|
   float.to_f
 end
 
-Given(/^an invoice with number "(.*?)"$/) do |invoice_number|
-  @invoice = Invoice.new invoice_number
+NUMBER = Transform /^[0-9]+$/ do |number|
+  number.to_i
 end
 
-Given(/^an activity "(.*?)" counting (.+) hours for the invoice "(.*?)"$/) do |name, hours, invoice_number|
-  activity = stub( name: name, hours: hours )
-  Invoice.find(invoice_number).add_entry activity
+INVOICE = Transform /^invoice ([0-9]+)$/ do |number|
+  Invoice.find number.to_i
 end
 
-Then(/^the total hours for invoice "(.*?)" should be (.*)$/) do |invoice_number, total_hours|
-   Invoice.find(invoice_number).total_hours.to_f.should eq total_hours.to_f
+Given(/^an invoice with number (#{NUMBER})$/) do |number|
+  @invoice = Invoice.new number
 end
 
-Given(/^an invoice "(.*?)" with VAT: (\d+)%, hourly rate: (\d+)$/) do |invoice_number, vat, hourly_rate|
-  Invoice.new invoice_number, vat: vat,  hourly_rate:  hourly_rate
+Given(/^an activity "(.*?)" counting (#{FLOAT}) hours for the (#{INVOICE})$/) do |name, hours, invoice|
+  activity = stub name: name, hours: hours
+  invoice.add_entry activity
 end
 
-Then(/^the total charged for invoice "(.*?)" without taxes should be (#{CAPTURE_FLOAT})$/) do |invoice_number, amount|
-  expect(  Invoice.find(invoice_number).ex_vat_total ).to eq amount
+Then(/^the total hours for (#{INVOICE}) should be (#{FLOAT})$/) do |invoice, total_hours|
+  expect( invoice.total_hours ).to eq total_hours
 end
 
-Then(/^the taxes for invoice "(.*?)" should be (#{CAPTURE_FLOAT})$/) do |invoice_number, amount|
-  expect(  Invoice.find(invoice_number).vat_total ).to eq amount
+Given(/^an invoice with number (#{NUMBER}), VAT: (#{NUMBER})%, hourly rate: (#{FLOAT})$/) do |number, vat, hourly_rate|
+  Invoice.new number, vat: vat, hourly_rate: hourly_rate
 end
 
-Then(/^the total charged for invoice "(.*?)" with taxes should be (#{CAPTURE_FLOAT})$/) do |invoice_number, amount|
-  expect(  Invoice.find(invoice_number).inc_vat_total ).to eq amount
+Then(/^the total charged for (#{INVOICE}) without taxes should be (#{FLOAT})$/) do |invoice, amount|
+  expect( invoice.ex_vat_total ).to eq amount
+end
+
+Then(/^the taxes for (#{INVOICE}) should be (#{FLOAT})$/) do |invoice, amount|
+  expect( invoice.vat_total ).to eq amount
+end
+
+Then(/^the total charged for (#{INVOICE}) with taxes should be (#{FLOAT})$/) do |invoice, amount|
+  expect( invoice.inc_vat_total ).to eq amount
 end
 
 Then(/^show me all invoices$/) do
@@ -39,5 +47,5 @@ Then(/^show me all invoices$/) do
 end
 
 Then(/^show me the invoice "(.*?)"$/) do |number|
-  puts (Invoice.find number).inspect
+  puts ( Invoice.find number ).inspect
 end
