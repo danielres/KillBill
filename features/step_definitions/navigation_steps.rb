5813b545b1ef69f5ce4d2c55683caaa7a243e8bd
Daniel Reszka <dannn.r@gmail.com>
2013-08-11 14:50:10 +0200

    fix bogus step

--- features/step_definitions/navigation_steps.rb
+++ features/step_definitions/navigation_steps.rb
@@ -1,3 +1,3 @@
-Then(/^I should see (\d+) invoices$/) do |qty|
-  expect( page ).to have_selector( ".invoice", count:3 )
+Then(/^I should see (\d+) invoices?$/) do |qty|
+  expect( page ).to have_selector( ".invoice", count: qty )
 end
